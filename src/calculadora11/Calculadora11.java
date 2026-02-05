11c4bdf1cea84cb0cddc52fb14b5f86bcb5772be
Ian Flores <ian.flores07@somo.eus>
2026-02-05 12:45:04 +0100

    Proyecto CalculadoraGIT11 clase Calculadora y corregir método dividir

--- src/calculadora11/Calculadora11.java
+++ src/calculadora11/Calculadora11.java
@@ -14,9 +14,10 @@ public class Calculadora11 {
 		return a * b; 
 	}  
 	
-	public double dividir(int a, int b) {
-		return (double) a / b; 
-	} 
+	public double dividir(int a, int b) {     if (b == 0) {
+		throw new IllegalArgumentException("No se puede dividir entre cero"); 
+    }     return (double) a / b; 
+} 
 
 	
 	
