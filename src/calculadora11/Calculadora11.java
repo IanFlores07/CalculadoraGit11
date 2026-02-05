25bf7bae1f2a8679fd7de8733404b722b06036b3
Ian Flores <ian.flores07@somo.eus>
2026-02-05 12:40:51 +0100

    Proyecto CalculadoraGIT11 clase Calculadora y método multiplicar y método dividir

--- src/calculadora11/Calculadora11.java
+++ src/calculadora11/Calculadora11.java
@@ -6,9 +6,18 @@ public class Calculadora11 {
 		 return a + b;
 		 }
 
-
-
 	public int restar(int a, int b) {
 		return a - b; 
 	}
+	
+	public int multiplicar(int a, int b) {
+		return a * b; 
+	}  
+	
+	public double dividir(int a, int b) {
+		return (double) a / b; 
+	} 
+
+	
+	
 } 
