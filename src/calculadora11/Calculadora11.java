c6275f08d9dc1833bd37a672d54846ca3bba8eec
Ian Flores <ian.flores07@somo.eus>
2026-02-11 11:03:15 +0100

    Añadidos comentarios de Javadoc

--- src/calculadora11/Calculadora11.java
+++ src/calculadora11/Calculadora11.java
@@ -3,19 +3,23 @@
 package calculadora11;
 
 public class Calculadora11 {
-
+	
+	/**Método sumar, recibe dos números y muestra la suma*/ 
 	public int sumar(int a, int b) {
 		 return a + b;
 		 }
-
+	
+	/**Método restas, recibe dos números y muestra la resta*/
 	public int restar(int a, int b) {
 		return a - b; 
 	}
 	
+	/**Método multiplicar, recibe dos números y muestra su multiplicación*/
 	public int multiplicar(int a, int b) {
 		return a * b; 
 	}  
 	
+	/**Método dividir, recibe dos números y muestra su división*/
 	public double dividir(int a, int b) {
 		if (b == 0) { 
         System.out.println("Error: división entre cero");
@@ -23,14 +27,14 @@ public class Calculadora11 {
     }     return (double) a / b; 
 } 
 
-	
+	/**Método potencia, recibe dos números una base y un exponente y muestra la operación*/
 	public int potencia(int base, int exponente) {
 		int resultado = 1;
 		for (int i = 0; i < exponente; i++) {
 			resultado *= base; 
     }     return resultado; 
    }	
-	
+	/**Método modulo, recibe dos números y devuelve el resto*/
 	public int modulo(int a, int b) {
 		return a % b; 
 } 
